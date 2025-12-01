6a16413decf7b8383e2318d28eb018454e6dda79
Maria-Liza-Perez <marializaperez40@gmail.com>
2025-12-01 23:25:46 +0800

    fix

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -72,7 +72,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |
 |	$autoload['libraries'] = array('database', 'email', 'session');
 */
-$autoload['libraries'] = array('database', 'pagination', 'form_validation', 'upload', 'session', 'email');
+$autoload['libraries'] = array('database', 'pagination', 'form_validation', 'upload', 'email');
 
 /*
 | -------------------------------------------------------------------
@@ -82,7 +82,7 @@ $autoload['libraries'] = array('database', 'pagination', 'form_validation', 'upl
 |
 |	$autoload['helpers'] = array('url', 'file');
 */
-$autoload['helpers'] = array('url', 'session');
+$autoload['helpers'] = array('url');
 
 /*
 | -------------------------------------------------------------------
